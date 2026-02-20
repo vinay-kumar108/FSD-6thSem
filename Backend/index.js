31a41cd412ad26a5053b6dd73177dc27ac66f322
vinay-kumar108 <vinay20010920@gmail.com>
2026-02-20 10:27:47 +0530

    accessing data using fileSystem at backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,51 +1,77 @@
-const http=require('http');
-const PORT=4004;
-const apidata=require('./apiCalling')
-const dataWrite=require('./usefsmodule')
-const server=http.createServer(async (req,res)=>{
+const http = require("http");
+const PORT = 4004;
+const apidata = require("./apiCalling");
+const { dataWrite, dataRead, deleteFile, readFileASync } = require('./usefsmodule');
+const server = http.createServer(async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type",
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // res.setHeader('Content-Type','application/json');
+  // res.end("<h2 style=color:red>Welcome to Node Server</h2>");
+  if (req.url == "/msg" && req.method == "GET") {
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h2>Greeting of the day!!!</h2>");
+  } else if (req.url == "/data" && req.method == "GET") {
+    res.setHeader("Content-Type", "application/json");
+    const jsondata = {
+      name: "rahul",
+      branch: "CS",
+      college: "ABES Engineering College",
+    };
+    res.end(JSON.stringify({ msg: jsondata }));
+  } else if (req.url == "/data" && req.method == "GET") {
+    res.setHeader("Content-Type", "application/json");
+    const jsondata = await apidata();
+    res.end(JSON.stringify({ msg: jsondata }));
+  } else if (req.url == "/datawrite" && req.method == "GET") {
+    res.setHeader("Content-Type", "application/json");
+    const jsondata = dataRead();
+    res.end(JSON.stringify({ msg: jsondata }));
+  } else if (req.url == "/dataread" && req.method == "GET") {
+    res.setHeader("Content-Type", "application/json");
+    const jsondata = dataRead();
+    res.end(JSON.stringify({ msg: jsondata }));
+  } else if (req.url == "/datadelete" && req.method == "GET") {
+    res.setHeader("Content-Type", "application/json");
+    const jsondata = deleteFile();
+    res.end(JSON.stringify({ msg: jsondata }));
+  }
+  else if (req.url == "/asyncread" && req.method == "GET") {
+    res.setHeader("Content-Type", "application/json");
+    const jsondata = await readFileASync();
+    res.end(JSON.stringify({ msg: jsondata }));
+  }
+  else if (req.url == "/register" && req.method == "POST") {
+    let arr=[];
+    let body='';
+    req.on('data',chunck=>{
+        body+=chunck
+    });
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-// res.setHeader('Content-Type','application/json');
-// res.end("<h2 style=color:red>Welcome to Node Server</h2>");
-if(req.url=="/msg" && req.method=="GET"){
-   res.setHeader("Content-Type","text/html");
-   res.end("<h2>Greeting of the day!!!</h2>") 
-}
-else if(req.url=="/data" && req.method=="POST"){
-res.setHeader("Content-Type","application/json");
-const jsondata={
-    name:"rahul",
-    branch:"CS",
-    college:"ABES Engineering College"
-}
-res.end(JSON.stringify({msg:jsondata}))
 
-}
-else if(req.url=="/data" && req.method=="GET"){
-res.setHeader("Content-Type","application/json");
-      const jsondata=await apidata();
-res.end(JSON.stringify({msg:jsondata}))
+    req.on('end',()=>{
+        const {name,email,password}= JSON.parse(body);
+        console.log(name+email+password);
+        res.setHeader("Content-Type", "application/json");
+res.end(JSON.stringify({msg:"student added successfully!!!!"}));
+    });
+  }
+  
+  
 
-}
+  else {
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h2 style=color:red>Invalid request</h2>");
+  }
+});
 
-else if(req.url=="/datawrite" && req.method=="GET"){
-res.setHeader("Content-Type","application/json");
-      const jsondata=dataWrite();
-res.end(JSON.stringify({msg:jsondata}))
-
-}
-
-else{
-res.setHeader("Content-Type","text/html");
-   res.end("<h2 style=color:red>Invalid request</h2>") 
-
-}
-
-})
-
-server.listen(PORT,()=>{
-    console.log("Server is listening on "+PORT)
-})
+server.listen(PORT, () => {
+  console.log("Server is listening on " + PORT);
+});
--- Backend/usefsmodule.js
+++ Backend/usefsmodule.js
@@ -1,10 +1,12 @@
 const fs=require('fs')
-const fs1=require('fs').promises
+const fs1=require('fs').promises;
 
 function dataWrite(){
     let statusmsg="";
     try{
-    fs.writeFileSync('studentdata.txt',"Welcome to FS module in NODE ")
+    fs.writeFileSync('studentdata.txt',`
+        Welcome to FS module in NODE  
+        its Vinay here`)
     console.log("Data written successfully!!!")
     statusmsg="Data written successfully!!!";
     }catch(e){
@@ -13,4 +15,44 @@ function dataWrite(){
     }
 return statusmsg
 }
-module.exports=dataWrite;
+
+function dataRead(){
+    let statusmsg="";
+    try{
+        const fdata=fs.readFileSync('studentdata.txt',{encoding:'utf-8'});
+        statusmsg=fdata;
+    }
+    catch(e){
+        statusmsg=e;
+    }
+    return statusmsg;
+};
+
+function deleteFile(){
+    let statusmsg="";
+    try{
+        fs.unlinkSync('studentdata.txt');
+        statusmsg = 'File deleted successfully';
+    }
+    catch(e){
+        statusmsg= 'error during deletion: '+e;
+    }
+    return statusmsg;
+};
+
+async function readFileASync(){
+    console.log('running');
+    try{
+         statusmsg=await fs1.readFile('student.json',{encoding:'utf-8'});
+
+    }catch(e){
+        statusmsg=e;
+
+    }
+    return statusmsg;
+}
+
+
+const obj={dataWrite,dataRead,deleteFile,readFileASync};
+
+module.exports=obj;
